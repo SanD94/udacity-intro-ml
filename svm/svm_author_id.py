1f271a5072ed8e2cd953b80f9a8e8cf8eb184e68
Safa ANDAC <sand94@Safas-MacBook-Pro.local>
2018-02-01 21:07:04 +0300

    SVM completed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,11 +19,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+#features_train = features_train[:len(features_train)//100] 
+#labels_train = labels_train[:len(labels_train)//100] 
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+clf = SVC(C=10000.)
+clf.fit(features_train, labels_train)
+labels_pred = clf.predict(features_test)
+
+print(accuracy_score(labels_test, labels_pred))
+print(len(labels_pred[labels_pred==1]))
 
 #########################################################
 
